883478ee2629ba1cbe5aaead4da98709ac0a7eb7
Brunna Brito <brunna.raphaela1@gmail.com>
2025-08-10 14:30:00 -0300

    first code

---
+++ P1- Semáforo/LED control.cpp
@@ -0,0 +1,16 @@
+// first c++ code
+int led = 13;//criação de uma variável para o pino 13 do led
+
+void setup()
+{
+  pinMode(led,OUTPUT);//define o modo do pino digital como saída
+}
+void loop()
+{
+digitalWrite(led,1);//coloca nível 1 no pino(liga o led)
+  
+  delay(2000);//aguarda 2 segundos
+  digitalWrite(led, 0);//coloca nivel 0 no pino(apaga o led)
+  
+    delay(2000);
+}
